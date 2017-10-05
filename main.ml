e36915f4a0316efcdedc3cbe5fae971fdb6c2dc3
tom-code <petrilak.tomas@gmail.com>
2017-10-05 23:55:36 +0200

    be good and close file

--- main.ml
+++ main.ml
@@ -7,11 +7,11 @@ let mmap_file name =
   let fd         = Unix.openfile name [Unix.O_RDONLY] 0 in
   let image_size = (Unix.stat name).Unix.st_size in
   Printf.printf "image_size=%d\n" image_size;
-  Bigarray.Genarray.map_file fd Bigarray.Char Bigarray.C_layout false (Array.of_list [image_size])
+  (fd, Bigarray.Genarray.map_file fd Bigarray.Char Bigarray.C_layout false (Array.of_list [image_size]))
 
 
 let() = 
-  let mm = mmap_file "b" in
+  let (fd, mm) = mmap_file "b" in
 
   let elf_info = Elf.read_info mm in
   Elf.dump_sections elf_info.Elf.sections;
@@ -21,5 +21,6 @@ let() =
   Printf.printf "---\n";
 
 
-  Dwarf.dump_info mm (Elf.section_find_offset_by_name elf_info.Elf.sections ".debug_info")
+  Dwarf.dump_info mm (Elf.section_find_offset_by_name elf_info.Elf.sections ".debug_info");
+  Unix.close fd
         
